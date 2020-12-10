be8321b7470fba2635646bfe6375382d35e8093b
Sanhita Patil <sanhitapatil591@gmail.com>
2020-12-10 20:23:18 +0530

    add new lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld{
     public static void main(String args[]) {
         System.out.println("Hello from test app");
+        System.out.println("Hello Jenkins");
+        System.out.println("Hello Jenkins");
+        System.out.println("Hello Jenkins");
+        System.out.println("Hello Jenkins");
+        System.out.println("Hello Jenkins");
+        System.out.println("Hello Jenkins");
+        
     }
 }
